ec7edc91e7a51926b8d3948e4b4e0d13e03dfd88
sourav78 <souravranjan488@gmail.com>
2024-05-25 13:44:19 +0530

    initial commit

---
+++ musicacademy/src/app/demoroute/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DemoRoute = () => {
+  return (
+    <>
+      <div className="">
+        <h2 className="text-2xl font-semibold text-center">Hello Sourav</h2>
+      </div>
+    </>
+  )
+}
+
+export default DemoRoute
---
+++ musicacademy/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-semibold text-center">Hello Next</h1>
+    </>
+  );
+}
