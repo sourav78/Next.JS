7ece20b04ffb194f3ebd9cee564b172e1d36148f
sourav78 <souravranjan488@gmail.com>
2024-05-28 08:41:04 +0530

    music academy - add featured card section

--- musicacademy/src/app/page.tsx
+++ musicacademy/src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedCources from "@/components/FeaturedCources";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <>
       <div className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
         <HeroSection/>
+        <FeaturedCources/>
       </div>
     </>
   );
--- musicacademy/src/components/HeroSection.tsx
+++ musicacademy/src/components/HeroSection.tsx
@@ -5,7 +5,7 @@ import { Button } from './ui/moving-border'
 
 const HeroSection = () => {
   return (
-    <div className='h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'>
+    <div className='md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0 min-h-screen'>
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
