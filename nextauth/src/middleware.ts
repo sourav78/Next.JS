86f6d670f2356c9d41d1d3bf08e6588b32535120
sourav78 <souravranjan488@gmail.com>
2024-06-19 08:29:37 +0530

    nextauth - add middleware for protect path

---
+++ nextauth/src/middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+
+    const path = request.nextUrl.pathname
+
+    const publicPath = path === "/auth/login" || path === "/auth/signup" || path === "/auth/verifyemail"
+
+    const token = request.cookies.get("token")?.value || ""
+
+    if(publicPath && token){
+        return NextResponse.redirect(new URL('/auth/profile', request.url))
+    }
+    
+    if(!publicPath && !token){
+        return NextResponse.redirect(new URL('/auth/login', request.url))
+    }
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/',
+    '/auth/login',
+    '/auth/signup',
+    '/auth/verifyemail',
+    '/auth/profile'
+  ],
+}
