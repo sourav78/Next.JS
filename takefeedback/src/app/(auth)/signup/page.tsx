9bfbe6f57df01760e4bac55a76cb4ca66e05f6f1
sourav78 <souravranjan488@gmail.com>
2024-07-09 08:37:12 +0530

    feedback - setting up signup page

--- takefeedback/src/app/(auth)/signup/page.tsx
+++ takefeedback/src/app/(auth)/signup/page.tsx
@@ -1,6 +1,29 @@
-import React from 'react'
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { useDebounceValue } from 'usehooks-ts'
+import { useToast } from "@/components/ui/use-toast"
+import { useRouter } from "next/navigation"
+
 
 const Signup = () => {
+
+  const { toast } = useToast()
+
+  const [username, setUsername] = useState("")
+  const [usernameMessage, setUsernameMessage] = useState("")
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const debouncedUsername = useDebounceValue(username, 300)
+
+  const router = useRouter()
+
+  //zod implementation
+
   return (
     <div>Signup</div>
   )
